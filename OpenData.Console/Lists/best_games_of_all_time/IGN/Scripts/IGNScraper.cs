4e08774bd832cb03399e6bf69afef47f5b334da4
André N. Darcie <andrendarcie@gmail.com>
2025-06-09 21:05:45 -0300

    Create IGNScraper.cs

---
+++ OpenData.Console/Lists/best_games_of_all_time/IGN/Scripts/IGNScraper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenData.Console.Lists.best_games_of_all_time.IGN.SCripts
+{
+    public class IGNScraper
+    {
+        private readonly string _url = "https://www.ign.com/articles/the-best-100-video-games-of-all-time";
+
+        public IGNScraper() { }
+
+        public string GetData()
+        {
+            return string.Empty;
+        }
+    }
+}
